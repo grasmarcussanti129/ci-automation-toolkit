84e882199762222a02ccd7655257a36a8c2d6e51
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-10-05 12:43:48 -0500

    Added error handling to setup function.

--- setup.js
+++ setup.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 function setup() {
-    console.log('Setting up your CI environment...');
-    // Additional setup logic here
+    try {
+        console.log('Setting up your CI environment...');
+        // Additional setup logic here
+    } catch (error) {
+        console.error('An error occurred during setup:', error);
+    }
 }
 
 setup();
